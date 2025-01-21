c823d7d41bd2e2cdc6eee5e04cdbbb352f44b3c1
SBParamanik <suryabrataparamanik07@gmail.com>
2025-01-21 10:18:56 +0530

    Add files via upload

---
+++ square root.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<math.h>
+int main ()
+{
+	float number,squareRoot;
+	printf ("enter the number");
+	scanf ("%f", &number);
+	squareRoot = sqrt(number);
+	printf("the square root of:%.2f",squareRoot);
+	
+}
