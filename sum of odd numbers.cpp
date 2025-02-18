e715eb56e9066581cc2e5ccf15ae4ee8dc96369d
SBParamanik <suryabrataparamanik07@gmail.com>
2025-02-18 11:21:11 +0530

    Add files via upload

---
+++ sum of odd numbers.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int s=0,i;
+	for(i=1;i<=20;i+=2)
+	{
+		s=s+i;
+	}
+	printf("%d",s);
+	return 0;
+}
---
+++ sum os squares using loop.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int s=0,i;
+	for(i=1;i<=10;i++)
+	{
+	 s=s+i*i;	
+	}
+	printf ("%d",s);
+	return 0;
+}
